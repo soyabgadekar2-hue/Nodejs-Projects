0008eac53e4fcbc577dfa3b009deb075cf8cfda9
Soyab <soyabgadekar2@gmail.com>
2026-08-13 11:04:06 +0530

    Adding first Student Details program

---
+++ task1.js
@@ -0,0 +1,17 @@
+console.log("Student Details:");
+let name = "Soyab";
+let age = 21;
+let course = "BCA";
+let marks = 75;
+
+if (marks >= 35) {
+    console.log("Result: Pass");
+} else {
+    console.log("Result: Fail");
+}
+
+console.log("Name: " + name);
+console.log("Age: " + age);
+console.log("Course: " + course);
+console.log("Marks: " + marks);
+console.log("Thank you for using our system!");
