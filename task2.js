281fff2d2e7b6280d03afe45dfb09c94dc2b34e5
Soyab <soyabgadekar2@gmail.com>
2026-08-13 11:11:04 +0530

    Adding basic maths operations

---
+++ task2.js
@@ -0,0 +1,17 @@
+let num1 = 10;
+let num2 = 20;
+
+let addition = num1 + num2;
+console.log("The sum of " + num1 + " and " + num2 + " is: " + addition);
+
+let substraction = num1 - num2;
+console.log("The subtraction of " + num1 + " and " + num2 + " is: " + substraction);
+
+let multiplication = num1 * num2;
+console.log("The multiplication of " + num1 + " and " + num2 + " is: " + multiplication);
+
+let division = num1 / num2;
+console.log("The division of " + num1 + " and " + num2 + " is: " + division);
+
+let modulus = num1 % num2;
+console.log("The modulus of " + num1 + " and " + num2 + " is: " + modulus);
